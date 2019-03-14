52a1a1b6a58b646c411b999c7a9502e41a5eb759
Kyle <kyle@kylemacquarrie.co.uk>
2019-03-14 22:42:05 +0000

    Extract dependencies

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+const title = 'Chapter 4: The wrong way to draw a circle'
+const slug = 'wrong-way-to-draw-a-circle'
+const path = require('path').resolve(__dirname)
+const config = require('@velvetkevorkian/sketch-boilerplate-p5').config
+
+module.exports = env => config(title, slug, path, env)
