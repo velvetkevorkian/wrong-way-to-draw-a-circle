f73ea1be5c8673006b8bac18062c3dcec6908c4c
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-06-03 23:37:19 +0100

    check in built files for gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const title = 'Chapter 4: The wrong way to draw a circle'
-const slug = 'wrong-way-to-draw-a-circle'
+const slug = 'docs'
 const path = require('path').resolve(__dirname)
 const config = require('@velvetkevorkian/sketch-boilerplate-p5').config
 
